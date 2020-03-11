5e8a5cbda89ba11b755fd99f23a2c5c6cf47cad2
Chantal S <chantal.shaib@gmail.com>
2020-03-10 22:07:56 -0400

    added menu.ipynb from Julia; added WIP intro to DS

---
+++ lessons/python/intro-data-analysis/mylibrary.py
@@ -0,0 +1,9 @@
+def sum_num(x: int, y: int) -> int:
+    ''' Simple function that sums two numbers x and y '''
+    return x + y
+
+
+def diff_num(x: int, y: int) -> int: 
+    ''' Simple function that sums two numbers x and y '''   
+    return x - y
+    
